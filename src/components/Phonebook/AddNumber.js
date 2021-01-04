f293bd138441b3bc571afe998964dfd3d812d17f
cultureweb <cedric.milius@yahoo.fr>
2021-01-04 10:14:13 +0100

    addNumber Component hardcoded data

--- src/components/Phonebook/AddNumber.js
+++ src/components/Phonebook/AddNumber.js
@@ -1,7 +1,36 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const AddNumber = () => {
-  return <div>AddNumber</div>;
-};
+  const [name, setName] = useState();
+  const [phone, setPhone] = useState();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(`submitted: ${name} - ${phone}`);
+  };
+
+  return (
+    <div>
+      <Link to="/">View phonebook</Link>
+      <h2>Add Number</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={({ target }) => setName(target.value)}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="Number"
+          onChange={({ target }) => setPhone(target.value)}
+        />
+        <br />
+
+        <button type="submit">Add number</button>
+      </form>
+    </div>
+  );
+};
 export default AddNumber;
