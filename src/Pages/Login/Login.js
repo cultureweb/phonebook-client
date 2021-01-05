aab7c462589a1a859ae098e388217c99c444c79f
cultureweb <cedric.milius@yahoo.fr>
2021-01-05 22:47:42 +0100

    auth/login to set token localstorage

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,50 @@
+import React, { useState } from "react";
+// import PropTypes from "prop-types";
+// import "./Login.css";
+
+async function loginUser(email, password) {
+  return fetch("http://localhost:42001/api/v1/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(email, password),
+  }).then(async (res) => {
+    if (res.status === 200) {
+      const data = await res.json();
+      if (
+        data &&
+        data.tokens &&
+        data.tokens.access &&
+        data.tokens.access.token
+      ) {
+        const token = data.tokens.access.token;
+        localStorage.setItem("token", token);
+        return token;
+      }
+    } else {
+      return res.text().then((error) => {
+        console.log("error", error);
+        return {
+          message: error,
+        };
+      });
+    }
+  });
+}
+
 const Login = () => {
-  const handleSubmit = (e) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  // const [token, seToken] = useState();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("handleSubmit");
+    const token = await loginUser({
+      email,
+      password,
+    });
+    // setToken(tokens);
   };
 
   return (
@@ -10,13 +53,13 @@ const Login = () => {
       <form onSubmit={handleSubmit}>
         <input
           type="text"
-          // onChange={({ target }) => setEmail(target.value)}
+          onChange={(e) => setEmail(e.target.value)}
           placeholder="Email"
         />
         <br />
         <input
           type="password"
-          // onChange={({ target }) => setPassword(target.value)}
+          onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
         />
         <br />
